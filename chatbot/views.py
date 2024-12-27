46a0e59e7d5efa952a37c9006146bb1ef9257862
olawa001 <abdullateefyusuf80@gmail.com>
2024-12-27 13:20:07 +0100

    third commit

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+import json
+import nltk
+import numpy as np
+import random
+import pickle
+import tensorflow as tf
+from nltk.stem import WordNetLemmatizer
+from django.http import JsonResponse
 
-# Create your views here.
+lemmatizer = WordNetLemmatizer()
+
+model = tf.keras.models.load_model('chatbot_model.h5')
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+
+with open('intents.json', 'r') as file:
+    intents = json.load(file)
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)    
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+def bag_of_words(sentence, words):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for s in sentence_words:
+        for i, w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+    return np.array(bag)            
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence, words)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return [{"intent": classes[r[0]], "probability": str(r[1])} for r in results]
+
+def get_response(intent_list, intent_json):
+    tag = intent_list[0]["intent"]
+    for i in intent_json["intents"]:
+        if i['tag'] == tag:
+            return random.choice(i['responses'])
+        
+
+# def chatbot_response(request):
+#     if request.method == 'POST':
+#         user_message = request.POST.get('message', '')
+#         predicted_intents = predict_class(user_message)
+#         response = get_response(predicted_intents, intents)
+#         return JsonResponse({"response": response})
+#     return JsonResponse({"error": "Invalid request"}, status=400)
+
+def chatbot_response(request):
+    if request.method == 'POST':
+        user_message = request.POST.get('message', '').strip()
+        if not user_message:
+            return JsonResponse({"error": "Message cannot be empty"}, status=400)
+        
+        try:
+            predicted_intents = predict_class(user_message)
+            response = get_response(predicted_intents, intents)
+            return JsonResponse({"response": response})
+        except FileNotFoundError as e:
+            return JsonResponse({"error": str(e)}, status=500)
+    return JsonResponse({"error": "Invalid request"}, status=400)
--- train_chatbot.py
+++ train_chatbot.py
@@ -1,26 +1,24 @@
 import json
 import numpy as np
-from sklearn import metrics
 import tensorflow as tf
 import random
 import nltk
-import pickle
 from nltk.stem import WordNetLemmatizer
-from sklearn.preprocessing import LabelEncoder
 
-nltk.download("punkt")
-nltk.download("wordnet")
+tf.config.set_visible_devices([], 'GPU')
+nltk.download("all")
+# nltk.download("wordnet")
 
 with open('intents.json', 'r') as file:
     intents = json.load(file)
 
-lemmatizer = WordNetLemmatizer()    
+lemmatizer = WordNetLemmatizer()
 words = []
 classes = []
 documents = []
 
 for intent in intents['intents']:
-    for pattern in intent['pattern']:
+    for pattern in intent['patterns']:
         word_list = nltk.word_tokenize(pattern)
         words.extend(word_list)
         documents.append((word_list, intent['tag']))
@@ -36,21 +34,20 @@ output_empty = [0] * len(classes)
 
 for document in documents:
     bag = []
-    word_patterns = documents[0]
+    word_patterns = document[0]  
     word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
     for w in words:
         bag.append(1 if w in word_patterns else 0)
 
-    output_row = list(output_empty)    
+    output_row = list(output_empty)
     output_row[classes.index(document[1])] = 1
     training.append([bag, output_row])
 
-random.shuffle(training)    
+random.shuffle(training)
 training = np.array(training, dtype=object)
 
 train_x = np.array(list(training[:, 0]))
 train_y = np.array(list(training[:, 1]))
-
 
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(128, input_shape=(len(train_x[0]),), activation='relu'),
@@ -64,6 +61,7 @@ model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accur
 model.fit(train_x, train_y, epochs=200, batch_size=8)
 model.save('chatbot_model.h5')
 
+import pickle
 
-pickle.dump(words, open('words.plk', 'wb'))
-pickle.dump(classes, open('classes.plk', 'wb'))
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
